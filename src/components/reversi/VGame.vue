8a3d8c098b6356aca0a929301159ab7f73f0271c
hiroyuki.abe.nu <hiroyuki.abe.nu@s1.nttdocomo.com>
2023-09-27 22:17:52 +0900

    ?

---
+++ src/components/reversi/VGame.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+        <VBoard/>
+    </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import VBoard from '@/components/reversi/VBoard.vue';
+
+@Component({
+    components: {
+        VBoard,
+    },
+})
+
+export default class VGame extends Vue {
+
+}
+</script>
